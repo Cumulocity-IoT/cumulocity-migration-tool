f1cc5e94dc3f9a3256f635177a42129f72644e2e
Christian Guether <christian.guether@softwareag.com>
2021-11-29 10:23:28 +0100

    update epl file selection

--- src/source/eplfiles/epl-files.component.ts
+++ src/source/eplfiles/epl-files.component.ts
@@ -15,54 +15,113 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
  */
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { IManagedObject } from '@c8y/client';
 import { DataService } from "../../data.service";
 import { SelectionService } from "../../selection.service";
 import { sortById } from "../../utils/utils";
 import { DataClient } from 'src/DataClient';
 import { UpdateableAlert } from "../../utils/UpdateableAlert";
-import { AlertService } from "@c8y/ngx-components";
+import { ActionControl, AlertService, Column, DataGridComponent, Pagination } from "@c8y/ngx-components";
 import { IEplFileConfiguration } from 'src/c8y-interfaces/IEplFileConfig';
+import { difference } from 'lodash';
 
 @Component({
     templateUrl: './epl-files.component.html'
 })
-export class EplFilesComponent {
+export class EplFilesComponent implements OnInit {
+    @ViewChild(DataGridComponent, { static: true })
+    dataGrid: DataGridComponent;
+
     private dataClient: DataClient;
 
-    allEplFiles: Promise<IEplFileConfiguration[]>;
+    allEplFiles: IEplFileConfiguration[];
 
-    constructor(private dataService: DataService, private selectionService: SelectionService, private alertService: AlertService) {
-        this.load();
-    }
+    selectedEPLFileIds: string[] = [];
 
-    load() {
-        this.dataClient = this.dataService.getSourceDataClient();
-        this.allEplFiles = this.dataClient.getEplFiles(true).then(eplFile => sortById(eplFile));
-    }
-
-    async toggleSelection(eplFile: IEplFileConfiguration) {
-        if (this.isSelected(eplFile)) {
-            this.selectionService.deselect(eplFile.id);
-        } else {
-            const alrt = new UpdateableAlert(this.alertService);
-            this.selectionService.select(eplFile.id);
-            alrt.update(`Epl file added for migration.`);
-            alrt.close(2000);
+    columns: Column[] = [
+        {
+            name: 'id',
+            header: 'ID',
+            path: 'id',
+            gridTrackSize: '0.5fr'
+        },
+        {
+            name: 'name',
+            header: 'Name',
+            path: 'name',
+            filterable: true,
+        },
+        {
+            name: 'state',
+            header: 'State',
+            path: 'state',
+            filterable: true,
         }
+    ];
+
+    pagination: Pagination = {
+        pageSize: 20,
+        currentPage: 1,
+    };
+
+    actionControls: ActionControl[] = [
+        {
+            text: 'View EPL file',
+            type: 'ACTION',
+            icon: 'image-file',
+            callback: ((eplFile: IEplFileConfiguration) => this.viewEplFile(eplFile))
+        }
+    ];
+
+    constructor(private dataService: DataService, private selectionService: SelectionService,
+        private alertService: AlertService) { }
+
+    async ngOnInit(): Promise<void> {
+        await this.load();
+        this.updateSelectedEPLFiles();
     }
 
-    isSelected(o: { id: string | number }) {
-        return this.selectionService.isSelected(o.id);
+    async load() {
+        this.dataClient = this.dataService.getSourceDataClient();
+        this.allEplFiles = await this.dataClient.getEplFiles(true).then(eplFile => sortById(eplFile));
     }
 
-    trackById(index, value) {
-        return value.id;
+    updateSelectedEPLFiles(): void {
+        this.allEplFiles.forEach((eplFile) => {
+            if (this.isSelected(eplFile.id)) {
+                this.selectedEPLFileIds.push(eplFile.id);
+            }
+        });
+
+        this.dataGrid.setItemsSelected(this.selectedEPLFileIds, true);
     }
 
-    viewEplFile(event: MouseEvent, eplFile: IEplFileConfiguration) {
-        event.stopPropagation();
+    async selectEPLFile(eplFileId: string): Promise<void> {
+        const alrt = new UpdateableAlert(this.alertService);
+        this.selectionService.select(eplFileId);
+        alrt.update(`Epl file added for migration.`);
+        alrt.close(2000);
+    }
+
+    isSelected(id: string): boolean {
+        return this.selectionService.isSelected(id);
+    }
+
+    onEPLFilesSelected(selectedEPLFileIds): void {
+        selectedEPLFileIds.forEach((selectedEPLFileId) => {
+            if (!this.isSelected(selectedEPLFileId)) {
+                this.selectEPLFile(selectedEPLFileId);
+            }
+        });
+
+        const eplFilesToDeselect = difference(this.selectedEPLFileIds, selectedEPLFileIds);
+        this.selectedEPLFileIds = selectedEPLFileIds;
+
+        eplFilesToDeselect.forEach(eplFileToDeselect => this.selectionService.deselect(eplFileToDeselect));
+    }
+
+    viewEplFile(eplFile: IEplFileConfiguration): void {
         const blob = new Blob([eplFile.contents], {
             type: 'text/plain'
         });
@@ -73,29 +132,5 @@ export class EplFilesComponent {
     reload() {
         this.dataClient.invalidateCache();
         this.load();
-    }
-
-    async selectAll() {
-        this.allEplFiles.then((eplFiles) => {
-            eplFiles.forEach(eplFile => {
-                if (this.isSelected(eplFile)) {
-                    return;
-                }
-
-                this.toggleSelection(eplFile);
-            });
-        });
-    }
-
-    async deselectAll() {
-        this.allEplFiles.then((eplFiles) => {
-            eplFiles.forEach(eplFile => {
-                if (!this.isSelected(eplFile)) {
-                    return;
-                }
-
-                this.toggleSelection(eplFile);
-            });
-        });
     }
 }
