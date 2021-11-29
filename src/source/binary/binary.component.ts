b18562f197caef07a00d0c0c7b1d822e2c393089
Christian Guether <christian.guether@softwareag.com>
2021-11-29 15:04:30 +0100

    update UI for binary selection

--- src/source/binary/binary.component.ts
+++ src/source/binary/binary.component.ts
@@ -15,64 +15,134 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
  */
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { IManagedObject } from '@c8y/client';
 import { DataService } from "../../data.service";
 import { SelectionService } from "../../selection.service";
 import { delay, sortById } from "../../utils/utils";
 import { UpdateableAlert } from "../../utils/UpdateableAlert";
-import { AlertService } from "@c8y/ngx-components";
+import { ActionControl, AlertService, Column, DataGridComponent, Pagination } from "@c8y/ngx-components";
 import { DataClient } from 'src/DataClient';
 import download from "downloadjs";
+import { difference } from 'lodash';
 
 @Component({
     templateUrl: './binary.component.html'
 })
-export class BinaryComponent {
-    private dataClient: DataClient;
-    allBinaries: Promise<IManagedObject[]>;
+export class BinaryComponent implements OnInit {
+    @ViewChild(DataGridComponent, { static: true })
+    dataGrid: DataGridComponent;
 
-    constructor(private dataService: DataService, private selectionService: SelectionService, private alertService: AlertService) {
-        this.load();
+    private dataClient: DataClient;
+
+    allBinaries: IManagedObject[];
+
+    selectedBinaryIds: string[] = [];
+
+    columns: Column[] = [
+        {
+            name: 'id',
+            header: 'ID',
+            path: 'id',
+            gridTrackSize: '0.5fr'
+        },
+        {
+            name: 'name',
+            header: 'Name',
+            path: 'name',
+            filterable: true,
+        },
+        {
+            name: 'owner',
+            header: 'Owner',
+            path: 'owner',
+            filterable: true,
+        },
+        {
+            name: 'size',
+            header: 'Size',
+            path: 'length',
+            sortable: false,
+            filterable: false,
+        }
+    ];
+
+    pagination: Pagination = {
+        pageSize: 20,
+        currentPage: 1,
+    };
+
+    actionControls: ActionControl[] = [
+        {
+            text: 'Download Binary',
+            type: 'ACTION',
+            icon: 'download-archive',
+            callback: ((binary: IManagedObject) => this.downloadBinary(binary))
+        },
+        {
+            text: 'View Managed Object',
+            type: 'ACTION',
+            icon: 'file',
+            callback: ((binary: IManagedObject) => this.viewManagedObject(binary))
+        }
+    ];
+
+    constructor(private dataService: DataService, private selectionService: SelectionService,
+        private alertService: AlertService) { }
+
+    async ngOnInit(): Promise<void> {
+        await this.load();
+        this.initSelectedBinaries();
     }
 
-    load() {
+    private async load() {
         this.dataClient = this.dataService.getSourceDataClient();
-        this.allBinaries = this.dataClient.getBinaries()
+        this.allBinaries = await this.dataClient.getBinaries()
             .then(binaries => binaries.filter(b => !b.hasOwnProperty('c8y_applications_storage')))
             .then(binaries => sortById(binaries));
     }
 
-    async toggleSelection(binary: IManagedObject) {
-        if (this.isSelected(binary)) {
-            this.selectionService.deselect(binary.id);
-        } else {
-            const alrt = new UpdateableAlert(this.alertService);
-            this.selectionService.select(binary.id);
-            alrt.update(`Searching for linked Groups and Devices...`);
-            const { groups, devices, simulators, smartRules, other, childParentLinks } = await this.dataClient.findLinkedFrom(binary);
-            childParentLinks.forEach(({ child, parent }) => {
-                this.selectionService.select(child, parent);
-            });
-            alrt.update(`Links found: ${groups.length} Groups, ${devices.length} Devices, ${simulators.length} Simulators, ${smartRules.length} Smart Rules, ${other.length} Other`);
-            alrt.close(5000);
-        }
+    private initSelectedBinaries(): void {
+        this.allBinaries.forEach((binary) => {
+            if (this.isSelected(binary.id)) {
+                this.selectedBinaryIds.push(binary.id);
+            }
+        });
+
+        this.dataGrid.setItemsSelected(this.selectedBinaryIds, true);
     }
 
-    isSelected(o: { id: string | number }) {
-        return this.selectionService.isSelected(o.id);
+    onBinariesSelected(selectedBinaryIds): void {
+        selectedBinaryIds.forEach((selectedBinaryId) => {
+            if (!this.isSelected(selectedBinaryId)) {
+                this.selectBinary(selectedBinaryId);
+            }
+        });
+
+        const binariesToDeselect = difference(this.selectedBinaryIds, selectedBinaryIds);
+        this.selectedBinaryIds = selectedBinaryIds;
+
+        binariesToDeselect.forEach(binaryToDeselect => this.selectionService.deselect(binaryToDeselect));
     }
 
-    trackById(index, value) {
-        return value.id;
+    private async selectBinary(binaryId: string) {
+        const alrt = new UpdateableAlert(this.alertService);
+        this.selectionService.select(binaryId);
+        alrt.update(`Searching for linked Groups and Devices...`);
+        const binary = this.allBinaries.find(binary => binary.id === binaryId);
+        const { groups, devices, simulators, smartRules, other, childParentLinks } = await this.dataClient.findLinkedFrom(binary);
+        childParentLinks.forEach(({ child, parent }) => {
+            this.selectionService.select(child, parent);
+        });
+        alrt.update(`Links found: ${groups.length} Groups, ${devices.length} Devices, ${simulators.length} Simulators, ${smartRules.length} Smart Rules, ${other.length} Other`);
+        alrt.close(5000);
     }
 
-    getName(binary: IManagedObject) {
-        return binary.name || '-';
+    private isSelected(id: string) {
+        return this.selectionService.isSelected(id);
     }
 
-    viewManagedObject(event: MouseEvent, managedObject: IManagedObject) {
-        event.stopPropagation();
+    private viewManagedObject(managedObject: IManagedObject) {
         const blob = new Blob([JSON.stringify(managedObject, undefined, 2)], {
             type: 'application/json'
         });
@@ -80,8 +150,7 @@ export class BinaryComponent {
         window.open(url, '_blank');
     }
 
-    async downloadBinary(event: MouseEvent, binary: IManagedObject) {
-        event.stopPropagation();
+    private async downloadBinary(binary: IManagedObject) {
         const downloadAlert = new UpdateableAlert(this.alertService);
         const blob = await this.dataClient.getBinaryBlob(binary, progress => {
             downloadAlert.update(`Downloading... ${(progress * 100).toFixed(0)}%`);
@@ -90,34 +159,5 @@ export class BinaryComponent {
         download(blob, binary.name);
         await delay(4000);
         downloadAlert.close();
-    }
-
-    reload() {
-        this.dataClient.invalidateCache();
-        this.load();
-    }
-
-    async selectAll() {
-        this.allBinaries.then((binaries) => {
-            binaries.forEach(binary => {
-                if (this.isSelected(binary)) {
-                    return;
-                }
-
-                this.toggleSelection(binary);
-            });
-        });
-    }
-
-    async deselectAll() {
-        this.allBinaries.then((binaries) => {
-            binaries.forEach(binary => {
-                if (!this.isSelected(binary)) {
-                    return;
-                }
-
-                this.toggleSelection(binary);
-            });
-        });
     }
 }
